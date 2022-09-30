14eac68c0c3f5bfad7d1116a928a5e38bc085b86
IsaacStephan317 <70538305+IsaacStephan317@users.noreply.github.com>
2022-09-30 00:48:57 -0400

    Trimmed Euchre game manager and worked on user input/output

--- Assets/Scripts/GameManagerEuchre.cs
+++ Assets/Scripts/GameManagerEuchre.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class GameManagerEuchre : MonoBehaviour
 {
+    public GameObject handcard1;
+    public GameObject handcard2;
+    public GameObject handcard3;
+    public GameObject handcard4;
+    public GameObject handcard5;
+    public GameObject handarea;
     public GameObject UserPlayer;
-    public GameObject Opponent1;
-    public GameObject PartnerPlayer;
-    public GameObject Opponent2;
-    public GameObject CardArea;
     public GameObject GameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Euchre Game Starting");
     }
 
     // Update is called once per frame
@@ -23,7 +25,22 @@ public class GameManagerEuchre : MonoBehaviour
         
     }
 
-    public void OnCLick() {
-        GameObject cardOne = Instantiate(UserPlayer, new Vector3(0,0,0), Quaternion.identity);
+    public void OnClick() {
+        Debug.Log("Deal button clicked");
+        GameObject card1Instance = Instantiate(handcard1, new Vector3(1, 0, 0), Quaternion.identity);
+        GameObject card2Instance = Instantiate(handcard2, new Vector3(-1, 0, 0), Quaternion.identity);
+        GameObject card3Instance = Instantiate(handcard3, new Vector3(1, 0, 0), Quaternion.identity);
+        GameObject card4Instance = Instantiate(handcard4, new Vector3(-1, 0, 0), Quaternion.identity);
+        GameObject card5Instance = Instantiate(handcard5, new Vector3(1, 0, 0), Quaternion.identity);
+
+        card1Instance.transform.SetParent(handarea.transform, false);
+        card2Instance.transform.SetParent(handarea.transform, false);
+        card3Instance.transform.SetParent(handarea.transform, false);
+        card4Instance.transform.SetParent(handarea.transform, false);
+        card5Instance.transform.SetParent(handarea.transform, false);
+
+        // card1Instance.transform.scale = new Vector3(0.5f, 0.5f, 0.5f);  
     }
 }
+
+
