4cfbb7558d2d90a15d8691d86cbc113b3d87df5d
SeanCThomas0 <thomassc@purdue.edu>
2022-09-30 01:29:05 -0400

    Added Launcher.cs
    reorganized branch with a folder and started writing scripts for testing

---
+++ Assets/Scenes/PhotonTest/Launcher.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace com.SeanCThomas0.CS307
+{
+
+    public class Launcher : MonoBehaviour
+    {
+        #region Private Serializable Fields
+
+
+        #endregion
+
+        #region Private Fields
+
+        ///<summary>
+        /// This client's version number. Group users by version number
+        /// </summary>
+        string gameVersion = "1";
+
+        #endregion
+
+
+    }
+}
