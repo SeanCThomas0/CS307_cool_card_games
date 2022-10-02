d74d8d94c5e7f6a12e159f950c278c74ff5bb00d
IsaacStephan317 <70538305+IsaacStephan317@users.noreply.github.com>
2022-10-02 11:45:15 -0400

    Created the basic framework and classes for Euchre game
    Developed a temporary card creation system for testing, as well as the classes to track the 4 users who are playing the game. Also developed a basic framework and outline to build the Euchre game logic including team assignments, and score checking with winner messages.

--- Assets/Scripts/GameManagerEuchre.cs
+++ Assets/Scripts/GameManagerEuchre.cs
@@ -13,10 +13,122 @@ public class GameManagerEuchre : MonoBehaviour
     public GameObject UserPlayer;
     public GameObject GameManager;
 
+    public class Card {
+        private int numbValue;
+        private string faceValue;
+        private string suit;
+
+        public Card(int numbValue, int suit) {
+            this.numbValue = numbValue;
+            if(numbValue == 11) {
+                this.faceValue = "Jack";
+            } else if(numbValue == 12) {
+                this.faceValue = "Queen";
+            } else if(numbValue == 13) {
+                this.faceValue = "King";
+            } else if(numbValue == 14) {
+                this.faceValue = "Ace";
+            } else {
+                this.faceValue = ("" + numbValue);
+            }
+
+            if(suit == 1) {
+                this.suit = "Clubs";
+            } else if(suit == 2) {
+                this.suit = "Diamonds";
+            } else if(suit == 3) {
+                this.suit = "Hearts";
+            } else if(suit == 4) {
+                this.suit = "Spades";
+            }
+        }
+
+        public int getNumbValue() {
+            return numbValue;
+        }
+
+        public string getFaceValue() {
+            return faceValue;
+        }
+        
+        public string getSuit() {
+            return suit;
+        }
+
+    }
+
+    public class CardPlayer {
+        private ArrayList hand;
+        private string userID;
+
+        public CardPlayer(string userID) {
+            hand = new ArrayList();
+            this.userID = userID;
+        }
+
+        public ArrayList getHandList() {
+            return hand;
+        }
+        
+        public string getUserID() {
+            return userID;
+        }
+
+        public Card playCard(int indexInHand) {
+            Card returnCard = (Card) hand[indexInHand - 1];
+            hand.RemoveAt(indexInHand - 1);
+            return returnCard;
+        }
+    
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Euchre Game Starting");
+        ArrayList cardDeck = new ArrayList();
+        ArrayList playerQueue = new ArrayList();
+
+        for(int i = 9; i <= 14; i++) {
+            for(int j = 1; j <= 4; j++) {
+                cardDeck.Add(new Card(i, j));
+            }
+        }
+        foreach(Card addedCard in cardDeck) {
+            string tempFace = addedCard.getFaceValue();
+            string tempSuit = addedCard.getSuit();
+            string message = "Added card: " + tempFace + " of " + tempSuit + "\n" ; 
+            Debug.Log(message);
+        }
+        CardPlayer[] teamOne = new CardPlayer[2];
+        CardPlayer[] teamTwo = new CardPlayer[2];
+        int oneCount = 0;
+        int twoCount = 0;
+
+        for(int i = 1; i <= 4; i++) {
+            playerQueue.Add(new CardPlayer("" + i));
+            if(i % 2 == 1) {
+                teamOne[oneCount] = (CardPlayer) playerQueue[i-1];
+                oneCount++;
+            }
+            if(i % 2 == 0) {
+                teamTwo[twoCount] = (CardPlayer) playerQueue[i-1];
+                twoCount++;
+            }
+        }
+        int teamOneScore = 0;
+        int teamTwoScore = 0;
+
+        while (teamOneScore <= 10 && teamTwoScore <= 10) {
+            teamOneScore++;
+        }
+        if(teamOneScore >= 10) {
+            string message = "Team 1 wins!";
+            Debug.Log(message);
+        } else if(teamTwoScore >= 10) {
+            string message = "Team 2 wins!";
+            Debug.Log(message);
+        }
     }
 
     // Update is called once per frame
