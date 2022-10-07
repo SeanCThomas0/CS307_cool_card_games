6d0ff125038106b2300466b42ef0dd5ac2295b2c
Juan Diaz <jdiazflo@purdue.edu>
2022-10-06 21:38:59 -0400

    Updated Solitaire script and added an UpdateSprite script to change back of card to front of card and vice versa

--- Assets/Scripts/Solitaire.cs
+++ Assets/Scripts/Solitaire.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Solitaire : MonoBehaviour
 {
+    public string[] suits = new string[] {"C", "D", "H", "S"};
+    public string[] values = new string[] {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
     public GameObject SolitaireGame;
-    CardDealer cardDealer;
     public GameObject cardPrefab;
 
-    public List<GameObject> deck;
+    public List<string> deck;
+
     // Start is called before the first frame update
     void Start()
     {
-        cardDealer = SolitaireGame.GetComponent<CardDealer>();
-        deck = cardDealer.RandomCards(52);
         SolitaireDeal();
     }
 
@@ -23,10 +23,37 @@ public class Solitaire : MonoBehaviour
         
     }
 
-    void SolitaireDeal() {
-        for (int i = 0; i < deck.Count; i++) {
-            GameObject newCard = Instantiate(deck[i], transform.position, Quaternion.identity);
-            
+    public void PlayCards() {
+        deck = GenerateDeck();
+    }
+
+    public static List<string> GenerateDeck() {
+        List<string> newDeck = new List<string>();
+        foreach (string s in suits) {
+            foreach (string v in values) {
+                newDeck.Add(s + v);
+            }
         }
+        return newDeck;
+    }
+
+    void Shuffle<T>(List<T> list) {
+        System.Random random = new System.Random();
+        int n = list.Count;
+        while (n > 1) {
+            int k = random.Next(n);
+            n--;
+            T temp list[k];
+            list[k] = list[n];
+            list[n] = temp;
+        }
+    }
+
+    void SolitaireDeal() {
+        float yOffset = 0;
+        float zOffset = 0.03f;
+        
+        yOffset = yOffset + 0.1f;
+        zOffset = zOffset + 0.03f;
     }
 }
--- Assets/UpdateSprite.cs
+++ Assets/UpdateSprite.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SolitaireUserInput : MonoBehaviour
+public class UpdateSprite : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
