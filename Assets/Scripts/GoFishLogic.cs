8750577fd4acf147a844f824640c90300665ab3c
Jonathan Maskew <jmaskew@purdue.edu>
2022-10-08 19:55:45 -0400

    Completed most of card game logic functions and researched how to interact with GUI

--- Assets/Scripts/GoFishLogic.cs
+++ Assets/Scripts/GoFishLogic.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 public class GoFishLogic : MonoBehaviour
 {
     public GameObject Controller;
-    CardDealer cardDealer;
 
-    List<Card> pool;
+    private CardDealer cardDealer;
+    private List<Card> pool;
+    private Player playerBot;
+    private Player playerSingle;
 
     // Start is called before the first frame update
     void Start()
     {
+        // set up deck of cards
         cardDealer = Controller.GetComponent<CardDealer>();
 
-       pool = cardDealer.RandomCards(52);
+        pool = cardDealer.RandomCards(52);
+
+        // create players
+        playerBot = new Player("0");
+        playerSingle = new Player("1");
+
+        // distribute cards for beginning of game
+        distributeCards(playerBot, 5);
+        distributeCards(playerSingle, 5);
+
+        for (int i = 0; i < playerBot.getHand().Count; i++) {
+            Debug.Log("Bot: " + playerBot.getHand()[i].getGameObject() + ", " + playerBot.getHand()[i].getNumValue() + ", " + playerBot.getHand()[i].getSuitValue());
+        }
+
+        for (int i = 0; i < playerSingle.getHand().Count; i++) {
+            Debug.Log("Player: " + playerSingle.getHand()[i].getGameObject() + ", " + playerSingle.getHand()[i].getNumValue() + ", " + playerSingle.getHand()[i].getSuitValue());
+        }
     }
 
     // Update is called once per frame
@@ -27,8 +46,14 @@ public class GoFishLogic : MonoBehaviour
         private string userID;
         private List<Card> hand;
 
+        private int[] numOfValues;
+        private List<Card> cardsOfFour;
+
         public Player(string userID) {
             this.userID = userID;
+            this.hand = new List<Card>();
+            this.numOfValues = new int[13];
+            this.cardsOfFour = new List<Card>();
         }
 
         public string getUserID() {
@@ -36,15 +61,77 @@ public class GoFishLogic : MonoBehaviour
         }
 
         public void addToHand(Card card) {
-
+            hand.Add(card);
+            // numOfValues[card.getNumValue()]++;
         }
 
         public void removeFromHand(Card card) {
+            hand.Remove(card);
+            // numOfValues[card.getNumValue()]--;
+        }
 
+        public int removeAllFromHand(int numValue) {
+            int numRemoved = 0;
+
+            for (int i = 0; i < hand.Count; i++) {
+                if (hand[i].getNumValue() == numValue) {
+                    hand.RemoveAt(i);
+                    numRemoved++;
+                    i--;
+                }
+            }
+            // numOfValues[numValue] = numOfValues[numValue] - numRemoved;
+            return numRemoved;
+        }
+
+        public int[] getNumOfValues() {
+            return numOfValues;
+        }
+
+        public List<Card> getHand() {
+            return hand;
         }
     }
 
-    public void addToHand_removeFromPool(Player player) {
+    // add cards to hand and remove from pool
+    public void distributeCards(Player player, int count) {
+        if (pool.Count < count) {
+            Debug.Log("Cannot distribute more cards than are available in the pool.");
+            return;
+        }
 
+        for (int i = 0; i < count; i++) {        
+            player.addToHand(pool[i]);
+            pool.RemoveAt(i);
+        }
     }
+
+    // remove cards from one player and give to another player
+    // returns true if the player had the cards
+    // false if the player did not have any cards
+    public bool requestCards(Player from, Player to, Card card) {
+        int numRemoved = from.removeAllFromHand(card.getNumValue());
+        for (int i = 0; i < numRemoved; i++) {
+            to.addToHand(card);
+        }
+
+        if (numRemoved > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+
+    // public List<Card> determineFourOfAKind(Player player) {
+    //     for (int i = 0; i < player.getNumOfValues().Length; i++) {
+    //         if (player.getNumOfValues()[i] >= 4) {
+    //             player.addCardOfFour();
+    //             return true;
+    //         }
+    //     }
+
+    //     return false;
+    // }
 }
