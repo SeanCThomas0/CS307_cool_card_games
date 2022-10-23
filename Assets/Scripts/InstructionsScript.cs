194edd376419877085ea1c005842d3e748a459a4
IsaacStephan317 <70538305+IsaacStephan317@users.noreply.github.com>
2022-10-23 02:17:33 -0400

    Linked button clicks to a text file for each card game that holds rules
    Added two text files for each card game in our application and had them appear in the rules section as text when a given game button is clicked.

--- Assets/Scripts/InstructionsScript.cs
+++ Assets/Scripts/InstructionsScript.cs
@@ -31,26 +31,42 @@ public class InstructionsScript : MonoBehaviour
     public void EuchreRules() {
         //display Euchre Rules in textBox
         Debug.Log("Euchre Button Pressed");
+        string text1 = System.IO.File.ReadAllText("Assets/GameTextFiles/EuchreTextpg1.txt");
+        string text2 = System.IO.File.ReadAllText("Assets/GameTextFiles/EuchreTextpg2.txt");
+        TextBox1.GetComponent<TMPro.TextMeshProUGUI>().text = text1;
+        TextBox2.GetComponent<TMPro.TextMeshProUGUI>().text = text2;
     }
 
     public void SolitareRules() {
         //display Solitare Rules in textBox
-        Debug.Log("Solitare Button Pressed");
+        string text1 = System.IO.File.ReadAllText("Assets/GameTextFiles/SolitaireTextpg1.txt");
+        string text2 = System.IO.File.ReadAllText("Assets/GameTextFiles/SolitaireTextpg2.txt");
+        TextBox1.GetComponent<TMPro.TextMeshProUGUI>().text = text1;
+        TextBox2.GetComponent<TMPro.TextMeshProUGUI>().text = text2;
     }
 
     public void GoFishRules() {
         //display GoFish Rules in textBox
-        Debug.Log("Go Fish Button Pressed");
+        string text1 = System.IO.File.ReadAllText("Assets/GameTextFiles/GoFishTextpg1.txt");
+        string text2 = System.IO.File.ReadAllText("Assets/GameTextFiles/GoFishTextpg2.txt");
+        TextBox1.GetComponent<TMPro.TextMeshProUGUI>().text = text1;
+        TextBox2.GetComponent<TMPro.TextMeshProUGUI>().text = text2;
     }
 
     public void PokerRules() {
         //display Poker Rules in textBox
-        Debug.Log("Poker Button Pressed");
+        string text1 = System.IO.File.ReadAllText("Assets/GameTextFiles/PokerTextpg1.txt");
+        string text2 = System.IO.File.ReadAllText("Assets/GameTextFiles/PokerTextpg2.txt");
+        TextBox1.GetComponent<TMPro.TextMeshProUGUI>().text = text1;
+        TextBox2.GetComponent<TMPro.TextMeshProUGUI>().text = text2;
     }
 
     public void BruhRules() {
         //display Bruh Rules in textBox
-        Debug.Log("Bruh Button Pressed");
+        string text1 = System.IO.File.ReadAllText("Assets/GameTextFiles/BruhTextpg1.txt");
+        string text2 = System.IO.File.ReadAllText("Assets/GameTextFiles/BruhTextpg2.txt");
+        TextBox1.GetComponent<TMPro.TextMeshProUGUI>().text = text1;
+        TextBox2.GetComponent<TMPro.TextMeshProUGUI>().text = text2;
     }
 
     public void UseExitButton() {
