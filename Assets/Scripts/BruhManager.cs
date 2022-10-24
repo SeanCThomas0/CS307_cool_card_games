d8956b89b3f957919cde961386112ee787fa4d88
IsaacStephan317 <70538305+IsaacStephan317@users.noreply.github.com>
2022-10-23 23:00:04 -0400

    Created the Bruh card game scene and the basic UI for the game

---
+++ Assets/Scripts/BruhManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BruhManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Bruh game Starting");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
