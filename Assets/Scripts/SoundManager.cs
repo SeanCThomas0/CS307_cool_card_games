29583b8f7e63bc5fe1a812e49f019514b95e1e99
SeanCThomas0 <thomassc@purdue.edu>
2022-10-23 21:27:16 -0400

    Created the audio event that plays click sound when button is pressed

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private TMPro.TextMeshProUGUI volumeSliderText = null;
+    [SerializeField] public GameObject clickSound;
+    [SerializeField] public AudioSource guh;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        LoadValues();
         
     }
 
@@ -15,4 +24,22 @@ public class SoundManager : MonoBehaviour
     {
         
     }
+
+    public void VolumeSlider(float volume) {
+        volumeSliderText.text=volume.ToString("0.0");
+
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue",volumeValue);
+        LoadValues();
+    }
+    void LoadValues() {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        guh.volume=volumeValue;
+
+
+        
+    }
+
+
 }
