8944f65b818b19b2c2ecaf5e254214ede31a63a5
gnlarsen <gnlarsen@purdue.edu>
2022-10-28 14:08:56 -0400

    Added rankings display current user
    Added a thing in the main menu in solitaire rankings that displays the current player's stats and ranking. If they don't have any stats for the rankings board they are viewing, their stats and rank will be "n/a"

--- Assets/Scripts/Rankings.cs
+++ Assets/Scripts/Rankings.cs
@@ -10,6 +10,7 @@ using Firebase.Unity;
 using Firebase.Auth;
 using static UnityEngine.UIElements.UxmlAttributeDescription;
 using System.Linq;
+using TMPro;
 
 public class Rankings : MonoBehaviour
 {
@@ -19,6 +20,13 @@ public class Rankings : MonoBehaviour
     private FirebaseAuth auth;
     public GameObject rankingsElement;
     public Transform rankingsContent;
+
+    public GameObject curUserRanking;
+    TextMeshProUGUI curUserRankingText;
+    public GameObject curUserUsername;
+    TextMeshProUGUI curUserUsernameText;
+    public GameObject curUserStat;
+    TextMeshProUGUI curUserStatText;
 
     // Start is called before the first frame update
     private void Start()
@@ -46,7 +54,30 @@ public class Rankings : MonoBehaviour
             databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
         }
 
+        //Set up text boxes for current user rankings
+        curUserRankingText = curUserRanking.GetComponent<TextMeshProUGUI>();
+        curUserUsernameText = curUserUsername.GetComponent<TextMeshProUGUI>();
+        curUserStatText = curUserStat.GetComponent<TextMeshProUGUI>();
 
+        //Get the current user's username
+        string curUsername = null;
+        databaseReference.Child("users").Child(auth.CurrentUser.UserId).Child("user_data/username").GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.Log("Rankings.cs curUsername canceled");
+            }
+            if (task.IsFaulted)
+            {
+                Debug.Log("Rankings.cs curUsername faulted");
+            }
+            else
+            {
+                curUsername = task.Result.Value.ToString();
+            }
+        });
+
+        //Get a list of all children with existing statistics
         var task = databaseReference.Child("users").OrderByChild("game_statistics/" + path).GetValueAsync();
 
         yield return new WaitUntil(predicate: () => task.IsCompleted);
@@ -70,7 +101,8 @@ public class Rankings : MonoBehaviour
             foreach (DataSnapshot childSnapshot in snapshot.Children.Reverse<DataSnapshot>())
             {
                 string username = childSnapshot.Child("user_data/username").Value.ToString();
-                Debug.Log(username);
+                
+                //Debug.Log(username);
                 if (childSnapshot.Child("game_statistics/" + path).Value == null)
                 {
                     break;
@@ -78,12 +110,29 @@ public class Rankings : MonoBehaviour
 
                 string stat = childSnapshot.Child("game_statistics/" + path).Value.ToString();
 
+                //Debug.Log(childSnapshot.Key + " " + auth.CurrentUser.UserId);
+
+                //If the current childSnapshot is the current user, set the current user data text to the user's values
+                if (childSnapshot.Key.Equals(auth.CurrentUser.UserId))
+                {
+                    curUserRankingText.text = order.ToString();
+                    curUserUsernameText.text = username;
+                    curUserStatText.text = stat;
+                }
+
                 GameObject rankingsBoardElement = Instantiate(rankingsElement, rankingsContent);
                 rankingsElement.GetComponent<RankingsElement>().NewRankingElement(order.ToString(), username, stat);
                 Debug.Log("Ranking added: " + order + " " + username + " " + stat);
                 order++;
             }
 
+            //If the current user wasn't in the childSnapshots, set the current user data text to null values
+            if (curUserUsernameText.Equals(curUsername))
+            {
+                curUserRankingText.text = "n/a";
+                curUserUsernameText.text = curUsername;
+                curUserStatText.text = "n/a";
+            }
 
             //These lines are accounting for some bs that doesn't allow the last player to be printed. You can ignore these for now
             GameObject blankElement = Instantiate(rankingsElement, rankingsContent);
