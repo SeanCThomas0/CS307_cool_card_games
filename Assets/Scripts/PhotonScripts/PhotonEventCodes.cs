99aac31ed24f3dbe9229bbca089a6ade76bf2977
SeanCThomas0 <thomassc@purdue.edu>
2022-11-16 18:28:55 -0500

    made subfolder for photon scripts

---
+++ Assets/Scripts/PhotonScripts/PhotonEventCodes.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EventCodes
+{
+    HostToClientData        =0,
+    StartGame               =1,
+    HostSendCardsToPlayer   =2,
+    GoFishFourOf            =3,
+    WinGame                 =4,
+    ExitGame                =5,
+    HostRemoveCardFromPlayer=6,
+    SyncCardsAtStart        =7
+}
