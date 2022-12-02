f8e9acb0136ff3069af6569cd8d9d9c3d60ba6cd
Juan Diaz <jdiazflo@purdue.edu>
2022-12-01 23:44:34 -0500

    adding pause and resume functions

--- Assets/Scripts/SolitaireUIButton.cs
+++ Assets/Scripts/SolitaireUIButton.cs
@@ -27,6 +27,14 @@ public class SolitaireUIButton : MonoBehaviour
         
     }
 
+    public void Pause() {
+        Time.timeScale = 0f;
+    }
+
+    public void Resume() {
+        Time.timeScale = 1f;
+    }
+
     public void PlayAgain() {
         highScorePanel.SetActive(false);
         ResetScene();
